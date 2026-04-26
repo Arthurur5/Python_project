b99bcbf17898ddff3098e5edb1e417a6e03005ea
Arthurur5 <abzalilov.arthur@gmail.com>
2026-04-26 11:41:07 +0300

    новая команда check

---
+++ main.py
@@ -0,0 +1,33 @@
+from rich.prompt import Prompt
+
+from manager import TaskManager
+
+
+manager = TaskManager()
+# manager.add_task("Включить ноутбук", 2)
+# manager.add_task("Начать делать проект", 3)
+# manager.add_task("Закончить делать проект", 3)
+# manager.add_task("Выключить ноутбук", 1)
+
+
+if __name__ == "__main__":
+    while True:
+        command = Prompt.ask(
+            "Введи команду",
+            choices=["show", "add", "delete", "stop", "check"],
+            default="show",
+        )
+        if command == "show":
+            manager.show_tasks()
+        elif command == "add":
+            title: str = Prompt.ask("Введи название")
+            priority: str = Prompt.ask("Введи приоритет")
+            manager.add_task(title, int(priority))
+        elif command == "delete":
+            id = int(Prompt.ask("Введи id"))
+            manager.delete_task(id)
+        elif command == "check":
+            id = int(Prompt.ask("Введи id"))
+            manager.check(id)
+        elif command == "stop":
+            break
--- manager.py
+++ manager.py
@@ -1,6 +1,5 @@
 from rich.console import Console
 from rich.table import Table
-from rich.prompt import Prompt
 from typing import List
 
 from task import Task
@@ -22,8 +21,13 @@ class TaskManager:
             if task.id == id:
                 self._tasks.remove(task)
 
-    def get_all(self) -> List[Task]:
-        return self._tasks.copy()
+    def check(self, id: int):
+        for task in self._tasks:
+            if task.id == id:
+                task.status = not task.status
+
+    # def get_all(self) -> List[Task]:
+    #     return self._tasks.copy()
 
     def show_tasks(self) -> None:
         table = Table(title="Мои задачи")
@@ -40,28 +44,3 @@ class TaskManager:
                 "1" if task.status else "0",
             )
         Console().print(table)
-
-
-manager = TaskManager()
-# manager.add_task("Включить ноутбук", 2)
-# manager.add_task("Начать делать проект", 3)
-# manager.add_task("Закончить делать проект", 3)
-# manager.add_task("Выключить ноутбук", 1)
-
-
-if __name__ == "__main__":
-    while True:
-        command = Prompt.ask(
-            "Введи команду", choices=["show", "add", "delete", "stop"], default="show"
-        )
-        if command == "show":
-            manager.show_tasks()
-        elif command == "add":
-            title = Prompt.ask("Введи название")
-            priority = Prompt.ask("Введи приоритет")
-            manager.add_task(title, int(priority))
-        elif command == "delete":
-            id = int(Prompt.ask("Введи id"))
-            manager.delete_task(id)
-        elif command == "stop":
-            break
