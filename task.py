f4a6a468fb3328217b60f2c72342f5872f5790ac
Arthurur5 <abzalilov.arthur@gmail.com>
2026-05-10 19:16:20 +0300

    pydantic теперь использую

--- task.py
+++ task.py
@@ -1,11 +1,10 @@
 """Модуль с классом Task"""
 
-from dataclasses import dataclass
+from pydantic import BaseModel, Field
 
 
-@dataclass
-class Task:
+class Task(BaseModel):
     id: int
-    title: str
-    priority: int
-    status: bool
+    title: str = Field(max_length=50)
+    priority: int = Field(ge=1, le=5)
+    status: bool = False
